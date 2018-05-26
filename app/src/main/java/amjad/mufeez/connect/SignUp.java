14aeddb002b4f39a90d84335abb0bded14ad4ba4
Mufeez Amjad <zeefumdajma@outlook.com>
2018-05-26 12:33:58 -0400

    Firebase Auth
    Login and Signup working

--- app/src/main/java/amjad/mufeez/connect/SignUp.java
+++ app/src/main/java/amjad/mufeez/connect/SignUp.java
@@ -1,13 +1,96 @@
 package amjad.mufeez.connect;
 
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class SignUp extends AppCompatActivity {
+
+    private FirebaseAuth mAuth;
+    private Button SignUpButton;
+    private EditText emailEdit;
+    private EditText passEdit;
+
+    private String email;
+    private String pass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        emailEdit = (EditText) findViewById(R.id.email);
+        passEdit = (EditText) findViewById(R.id.password);
+
+        SignUpButton = (Button) findViewById(R.id.button);
+        SignUpButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                email = emailEdit.getText().toString();
+                pass = passEdit.getText().toString();
+
+                if (email.length() > 1 && pass.length() > 1) {
+
+                    mAuth.createUserWithEmailAndPassword(email, pass)
+                            .addOnCompleteListener(SignUp.this, new OnCompleteListener<AuthResult>() {
+                                @Override
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    if (task.isSuccessful()) {
+                                        // Sign in success, update UI with the signed-in user's information
+                                        FirebaseUser user = mAuth.getCurrentUser();
+
+                                        SharedPreferences sharedPref = SignUp.this.getPreferences(Context.MODE_PRIVATE);
+                                        SharedPreferences.Editor editor = sharedPref.edit();
+                                        editor.putString("UID", user.getUid());
+                                        editor.commit();
+
+                                        openSignUp();
+
+                                    } else {
+                                        // If sign in fails, display a message to the user.
+                                        Toast.makeText(SignUp.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+                                        //updateUI(null);
+                                    }
+
+                                    // ...
+                                }
+                            });
+                }
+            }
+        });
     }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        //updateUI(currentUser);
+    }
+
+    public void openSignUp(){
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+
+        overridePendingTransition(R.anim.enter, R.anim.exit);
+    }
+
+
 }
